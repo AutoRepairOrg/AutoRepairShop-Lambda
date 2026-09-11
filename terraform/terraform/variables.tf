0ddc8b00832ea6885a24add4563b6c70b6fe682e
dhiulia <85645860+dhiuliaantunes@users.noreply.github.com>
2026-09-11 16:28:05 -0300

    Delete terraform/terraform/variables.tf

--- terraform/terraform/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "api_nlb_dns" {
-  description = "DNS do NLB que expõe a API no EKS"
-  type        = string
-
-  default = "k8s-oficina-apinlb-d48300ca48-b2aee7cea5a456da.elb.us-east-1.amazonaws.com"
-}
