3ebf9578ecd13995c101d7a93c75b84e9b2d0836
dhiulia <85645860+dhiuliaantunes@users.noreply.github.com>
2026-09-11 19:03:47 -0300

    Update Customer.cs

--- src/AutoRepairShop.Login/Models/Customer.cs
+++ src/AutoRepairShop.Login/Models/Customer.cs
@@ -2,9 +2,8 @@ namespace AutoRepairShop.Login.Models;
 
 public class Customer
 {
-    public int Id { get; set; }
-    public string Cpf { get; set; } = string.Empty;
+    public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public string Cpf { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
-    public bool IsActive { get; set; }
 }
