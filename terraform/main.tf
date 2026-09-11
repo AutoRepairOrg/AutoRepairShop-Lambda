5c10d0ce7ea86deee91a0aa660e95da69575e76b
dhiulia <85645860+dhiuliaantunes@users.noreply.github.com>
2026-09-11 16:28:54 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-```hcl
 terraform {
   required_providers {
     aws = {
@@ -262,4 +261,3 @@ output "login_endpoint" {
 output "api_endpoint" {
   value = "${aws_api_gateway_stage.prod.invoke_url}/api"
 }
-```
