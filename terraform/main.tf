225346bfe7aea382d9c3d5d7cc61564eb00645c1
dhiulia <85645860+dhiuliaantunes@users.noreply.github.com>
2026-09-12 15:31:21 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,25 +72,6 @@ provider "kubectl" {
 }
 
 # ============================================================
-# VARIABLES
-# ============================================================
-
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
-
-variable "eks_cluster_name" {
-  type    = string
-  default = "autorepairshop-eks"
-}
-
-variable "jwt_secret_key" {
-  type      = string
-  sensitive = true
-}
-
-# ============================================================
 # DATA SOURCES
 # ============================================================
 
