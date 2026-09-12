19fac2357588828b8b8eb3ce6d45deff42f9ae01
dhiulia <85645860+dhiuliaantunes@users.noreply.github.com>
2026-09-12 14:47:48 -0300

    Update Customer.cs

--- src/AutoRepairShop.Login/Models/Customer.cs
+++ src/AutoRepairShop.Login/Models/Customer.cs
@@ -2,11 +2,8 @@ namespace AutoRepairShop.Login.Models;
 
 public class Customer
 {
-       public Guid Id { get; private set; }
-    public string Name { get; private set; } = string.Empty;
-    public Document Document { get; private set; } = null!;
-    public string Phone { get; private set; } = string.Empty;
-    public string Email { get; private set; } = string.Empty;
-    public string Username { get; private set; } = string.Empty;
-    public string Password { get; private set; } = string.Empty;
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Cpf { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
 }
