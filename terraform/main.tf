2526b370611cde305fb73fa0ca398a822e06bb0e
dhiulia <85645860+dhiuliaantunes@users.noreply.github.com>
2026-09-12 13:07:50 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "s3" {
+    bucket = "autorepairshop-terraform-state-lab"
+    key    = "lambda/terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
