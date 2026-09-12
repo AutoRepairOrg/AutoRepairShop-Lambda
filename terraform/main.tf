5e86d2b2b8615ce57e015436a679ba9f69d59386
mateusnpinheiro95 <mateusnpinheiro95@gmail.com>
2026-09-12 18:07:33 -0300

    style(terraform): apply terraform fmt to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -342,7 +342,7 @@ resource "aws_api_gateway_deployment" "prod" {
 }
 
 resource "aws_api_gateway_stage" "prod" {
-  rest_api_id    = aws_api_gateway_rest_api.autorepair_api.id
-  deployment_id  = aws_api_gateway_deployment.prod.id
-  stage_name     = "prod"
+  rest_api_id   = aws_api_gateway_rest_api.autorepair_api.id
+  deployment_id = aws_api_gateway_deployment.prod.id
+  stage_name    = "prod"
 }
